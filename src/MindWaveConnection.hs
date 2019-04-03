e66981d4a543f5f9e25fa5cc11f682d0501cce59
Sandy Maguire <sandy@sandymaguire.me>
2019-04-02 20:16:17 -0400

    types and cleanup

--- src/MindWaveConnection.hs
+++ src/MindWaveConnection.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
 module MindWaveConnection (
  readMind,
@@ -7,17 +8,15 @@ module MindWaveConnection (
  disconnect
 ) where
 
-import System.Hardware.Serialport (SerialPort)
-import qualified System.Hardware.Serialport as SP
-import GHC.IO.Handle
-import Foreign
-import qualified Data.ByteString as B
-import Data.Word
-import Data.Char (intToDigit, toUpper)
 import qualified Control.Exception as Ex
-import Control.Monad.State
-import System.Console.ANSI
-import Data.IORef
+import           Control.Monad.State
+import           Data.Bits
+import qualified Data.ByteString as B
+import           Data.Char (intToDigit, toUpper)
+import           Data.IORef
+import           Data.Word
+import           System.Hardware.Serialport (SerialPort)
+import qualified System.Hardware.Serialport as SP
 
 -- | The default location the MindWave serial port is mounted (on my linux system)
 mindWaveDev :: FilePath
@@ -26,11 +25,6 @@ mindWaveDev = "/dev/ttyUSB0"
 -- | The serial port settings required in the MindWave spec
 mindWaveSerialSettings :: SP.SerialPortSettings
 mindWaveSerialSettings = SP.defaultSerialSettings { SP.commSpeed = SP.CS115200 }
-
--- | Sends the "magic word" for starting a connection to the MindWave.
---   Telling the MindWave to start sending data.
-sendConnect :: Word8 -> Word8 -> SerialPort -> IO Int
-sendConnect a b sp = SP.send sp (B.pack [0xC0,a,b])
 
 -- | Sends the "magic word" for ending a connection to the MindWave.
 --   Telling the MindWave to stop sending data.
@@ -98,25 +92,17 @@ checksum bs =  complement (foldr (+) 0 bs)
 -- | A packet of information from the MindWave is simple a list of Word8
 data MindWavePacket = MindWavePacket [Word8]
 
-{-
-instance Show MindWavePacket where
- show (MindWavePacket pl) = "0xAA 0xAA " ++ showWord8 (fromIntegral $ length pl) ++ concat (map (\b -> ' ':showWord8 b) pl) ++ " " ++ showWord8 (checksum pl)
-
-printPackets :: MindWave ()
-printPackets = sequence_ $ repeat $ readMindWavePacket >>= lift . print
--}
-
 -- | A function for reading a packet of information from the MindWave
 readMindWavePacket :: MindWave MindWavePacket
 readMindWavePacket = do
  sync <- readByte
  case sync of
    0xAA -> do
-     sync <- readByte
-     case sync of
+     sync' <- readByte
+     case sync' of
        0xAA -> readPayload
-       other -> readMindWavePacket
-   other -> readMindWavePacket
+       _ -> readMindWavePacket
+   _ -> readMindWavePacket
 
 -- | A helper function for reading the payload from the MindWave
 readPayload :: MindWave MindWavePacket
@@ -202,10 +188,10 @@ prettyPrint mwp@(MindWaveDataRow excode code value) =
         0x05 -> "MEDITATION eSense: " ++ show (head value)
         0x16 -> "Blink Strength: " ++ show (head value)
         0x55 -> "EXCODE " ++ show value
-	0x80 -> "RAW: " ++ hexValue 2 value
-	0x83 -> "ASIC_EEG_POWER: " ++ eegValue value
-	0xAA -> "SYNC " ++ show value
-	0xD0 -> "Connected to Headset " ++ hexValue 2 value
+  0x80 -> "RAW: " ++ hexValue 2 value
+  0x83 -> "ASIC_EEG_POWER: " ++ eegValue value
+  0xAA -> "SYNC " ++ show value
+  0xD0 -> "Connected to Headset " ++ hexValue 2 value
         0xD1 -> case (length value) of
                  0 -> "No Headsets found"
                  2 -> "Headset " ++ hexValue 2 value ++ " not found"
@@ -215,13 +201,20 @@ prettyPrint mwp@(MindWaveDataRow excode code value) =
         0xD4 -> case (head value) of
                  0 -> "Dongle in Standy Mode"
                  _ -> "Scanning for Headset"
-	_ -> show mwp
+  _ -> show mwp
   _ -> show mwp
 -}
 
 -- | A helper function for pretty printing Word8 in Hex format
 hexValue :: Int -> [Word8] -> String
 hexValue n ws = "0x" ++ concat (map (\w -> drop 2 (showWord8 w)) (take n ws))
+
+
+buildWord32 :: Word8 -> Word8 -> Word8 -> Word32
+buildWord32 b c d =
+  (flip shift 16 $ fromIntegral b) .|.
+  (flip shift 8  $ fromIntegral c) .|.
+   fromIntegral d
 
 {-
 eegValue :: [Word8] -> String
@@ -242,92 +235,84 @@ eegValue ws = "error " ++ show ws
 data MindWaveInfo = MindWaveInfo
   { dongle_status  :: String
   , last_message   :: String
-  , poor_signal    :: String
-  , attention      :: String
-  , meditation     :: String
-  , blink_strength :: String
+  , poor_signal    :: Word8
+  , attention      :: Word8
+  , meditation     :: Word8
+  , blink_strength :: Word8
   , raw_value      :: String
-  , delta          :: String
-  , theta          :: String
-  , low_alpha      :: String
-  , high_alpha     :: String
-  , low_beta       :: String
-  , high_beta      :: String
-  , low_gamma      :: String
-  , mid_gamma      :: String
+  , delta          :: Float
+  , theta          :: Float
+  , low_alpha      :: Float
+  , high_alpha     :: Float
+  , low_beta       :: Float
+  , high_beta      :: Float
+  , low_gamma      :: Float
+  , mid_gamma      :: Float
   , unknown_code   :: String
   } deriving Show
 
-{-
-instance Show MindWaveInfo where
- show mwi =
-  "dongle_status:  " ++ dongle_status mwi ++ "\n" ++
-  "last_message:   " ++ last_message mwi ++ "\n" ++
-  "poor_signal:    " ++ poor_signal mwi ++ "\n" ++
-  "attention:      " ++ attention mwi ++ "\n" ++
-  "meditation:     " ++ meditation mwi ++ "\n" ++
-  "blink_strength: " ++ blink_strength mwi ++ "\n" ++
-  "raw_value:      " ++ raw_value mwi ++ "\n" ++
-  "delta:          " ++ delta mwi ++ "\n" ++
-  "theta:          " ++ theta mwi ++ "\n" ++
-  "low_alpha:      " ++ low_alpha mwi ++ "\n" ++
-  "high_alpha:     " ++ high_alpha mwi ++ "\n" ++
-  "low_beta:       " ++ low_beta mwi ++ "\n" ++
-  "high_beta:      " ++ high_beta mwi ++ "\n" ++
-  "low_gamma:      " ++ low_gamma mwi ++ "\n" ++
-  "mid_gamma:      " ++ mid_gamma mwi ++ "\n" ++
-  "unknown_code:   " ++ unknown_code mwi ++ "\n"
--}
-
 -- | Before connecting to the MindWave, we have no information.
 initialMindWaveInfo :: MindWaveInfo
-initialMindWaveInfo = MindWaveInfo "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" ""
+initialMindWaveInfo = MindWaveInfo "" "" 0 0 0 0 "" 0 0 0 0 0 0 0 0 ""
 
 -- | For any data row we recieve, we can update the info accordingly
 updateState :: MindWaveDataRow -> MindWaveInfo -> MindWaveInfo
 updateState mwp@(MindWaveDataRow excode code value) mwi =
   case excode of
-   0 -> case code of
-         0x02 -> mwi { poor_signal = show (head value) ++ "  "}
-         0x04 -> mwi { attention = show (head value) ++ "  "}
-         0x05 -> mwi { meditation = show (head value) ++ "  "}
-         0x16 -> mwi { blink_strength = show (head value) ++ "  "}
-	 0x80 -> mwi { raw_value = hexValue 2 value }
-	 0x83 -> updateEegValue value mwi
-	 0xD0 -> mwi { dongle_status = "Connected to Headset " ++ hexValue 2 value }
-         0xD1 -> case (length value) of
-                  0 -> mwi { last_message = "No Headsets found                 " }
-                  2 -> mwi { last_message = "Headset " ++ hexValue 2 value ++ " not found" }
-                  _ -> mwi { unknown_code = show mwp }
-         0xD2 -> mwi { last_message = "Disconnected from Headset " ++ hexValue 2 value }
-         0xD3 -> mwi { last_message = "Request Denied" }
-         0xD4 -> case (head value) of
-                  0 -> mwi { dongle_status = "Dongle in Standy Mode" }
-                  _ -> mwi { dongle_status = "Scanning for Headset " }
-	 _ -> mwi { unknown_code = show mwp }
+   0 ->
+     case code of
+       0x02 -> mwi { poor_signal = head value }
+       0x04 -> mwi { attention = head value }
+       0x05 -> mwi { meditation = head value }
+       0x16 -> mwi { blink_strength = head value }
+       0x80 -> mwi { raw_value = hexValue 2 value }
+       0x83 -> updateEegValue value mwi
+       0xD0 -> mwi { dongle_status = "Connected to Headset " ++ hexValue 2 value }
+       0xD1 ->
+         case (length value) of
+           0 -> mwi { last_message = "No Headsets found                 " }
+           2 -> mwi { last_message = "Headset " ++ hexValue 2 value ++ " not found" }
+           _ -> mwi { unknown_code = show mwp }
+       0xD2 -> mwi { last_message = "Disconnected from Headset " ++ hexValue 2 value }
+       0xD3 -> mwi { last_message = "Request Denied" }
+       0xD4 ->
+         case (head value) of
+           0 -> mwi { dongle_status = "Dongle in Standy Mode" }
+           _ -> mwi { dongle_status = "Scanning for Headset " }
+       _ -> mwi { unknown_code = show mwp }
    _ -> mwi { unknown_code = show mwp }
 
 -- | A helper function for extracting EEG specific values from the MindWave
 updateEegValue :: [Word8] -> MindWaveInfo -> MindWaveInfo
-updateEegValue [d1,d2,d3,t1,t2,t3,la1,la2,la3,ha1,ha2,ha3,lb1,lb2,lb3,hb1,hb2,hb3,lg1,lg2,lg3,mg1,mg2,mg3] mwi = mwi {
- delta = d,
- theta = t,
- low_alpha = la,
- high_alpha = ha,
- low_beta = lb,
- high_beta = hb,
- low_gamma = lg,
- mid_gamma = mg
-}
+updateEegValue
+  [ d1,  d2,  d3
+  , t1,  t2,  t3
+  , la1, la2, la3
+  , ha1, ha2, ha3
+  , lb1, lb2, lb3
+  , hb1, hb2, hb3
+  , lg1, lg2, lg3
+  , mg1, mg2, mg3
+  ] mwi
+  = mwi
+      { delta = d
+      , theta = t
+      , low_alpha = la
+      , high_alpha = ha
+      , low_beta = lb
+      , high_beta = hb
+      , low_gamma = lg
+      , mid_gamma = mg
+      }
  where
-  d = hexValue 3 [d1,d2,d3]
-  t = hexValue 3 [t1,t2,t3]
-  la = hexValue 3 [la1,la2,la3]
-  ha = hexValue 3 [ha1,ha2,ha3]
-  lb = hexValue 3 [lb1,lb2,lb3]
-  hb = hexValue 3 [hb1,hb2,hb3]
-  lg = hexValue 3 [lg1,lg2,lg3]
-  mg = hexValue 3 [mg1,mg2,mg3]
+  d  = log $ fromIntegral $ buildWord32 d1 d2 d3
+  t  = log $ fromIntegral $ buildWord32 t1 t2 t3
+  la = log $ fromIntegral $ buildWord32 la1 la2 la3
+  ha = log $ fromIntegral $ buildWord32 ha1 ha2 ha3
+  lb = log $ fromIntegral $ buildWord32 lb1 lb2 lb3
+  hb = log $ fromIntegral $ buildWord32 hb1 hb2 hb3
+  lg = log $ fromIntegral $ buildWord32 lg1 lg2 lg3
+  mg = log $ fromIntegral $ buildWord32 mg1 mg2 mg3
 updateEegValue ws mwi = mwi { unknown_code = show ws }
 
 -- | Store MindWaveInfo in an IORef, and continually update it from the MindWave
